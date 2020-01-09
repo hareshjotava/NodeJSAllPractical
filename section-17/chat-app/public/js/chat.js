b0ca26df56693248c910346f06e06df5950bba7d
Haresh Jotava <jotavaha@SYNOVERGETECH.COM>
2020-01-09 12:15:09 +0530

    commit

--- section-17/chat-app/public/js/chat.js
+++ section-17/chat-app/public/js/chat.js
@@ -18,7 +18,14 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message,(error)=>{
+
+        if(error)
+        {
+            return console.log(error)
+        }
+        console.log('message was delivered successfully! ')
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
@@ -30,6 +37,8 @@ document.querySelector('#send-location').addEventListener('click', () => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        },()=>{
+            console.log('location shared!')
         })
     })
 })
--- section-17/chat-app/src/index.js
+++ section-17/chat-app/src/index.js
@@ -2,12 +2,13 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter=require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 7000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
@@ -27,12 +28,22 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', (message) => {
+   
+    socket.on('sendMessage', (message,callback) => {
+        const filter=new Filter()
+
+        if(filter.isProfane(message)) 
+        {
+            return callback('profane is not allowed!')
+        }
+
         io.emit('message', message)
+        callback('delivered!');
     })
 
-    socket.on('sendLocation', (coords) => {
+    socket.on('sendLocation', (coords,callback) => {
         io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback();
     })
 
     socket.on('disconnect', () => {
